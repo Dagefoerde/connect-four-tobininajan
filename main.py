298087d680529e1b4bd751976efac6295eec465a
n_herr03 <nina.herrmann@uni-muenster.de>
2020-02-29 14:42:37 +0100

    save maximum

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ b = tf.Variable(initial_value=b_init(shape=(7,), dtype='float32'), trainable=Tru
 bot2 = ConnectFourBotRandom()
 bot1 = ConnectFourBotNN(w, b)
 
-numberiter = 100
-number_ga_configs = 50
+numberiter = 2
+number_ga_configs = 2
 results_log = np.array([])
 gameswon = 0
 gamesundecided = 0
-
-for x in range (number_ga_configs):
+maximum = np.array([]);
+for x in range(number_ga_configs):
     for x in range(numberiter):
         board = ConnectFourEnv()
+        bot1.reset()
+        bot2.reset()
         current_player = 1
         while not(board.is_win_state()):
             if current_player == 1:
@@ -35,24 +37,20 @@ for x in range (number_ga_configs):
                bot2.inform(move)
             if len(board.available_moves()) == 0:
                 break
-        print(board.board)
+        #print(board.board)
         if board.is_win_state() & current_player*-1 == 1:
             gameswon += 1
         else:
             gamesundecided += 1
     if gamesundecided != numberiter:
-        results_log.append(np.array(
-            np.array(bot1.NN.w),
-            np.array(bot1.NN.b),
-            gameswon/(numberiter-gamesundecided)))
-maximum = results_log[0]
+        newresult = np.array([bot1.NN.w, bot1.NN.b, gameswon/(numberiter-gamesundecided)])
+        if len(maximum) == 0:
+            maximum = newresult
+        else:
+            if maximum[2] < gameswon/(numberiter-gamesundecided):
+                maximum = newresult
 
-for item in results_log:
-    if item[2] > maximum[2]:
-        maximum = item
-
-np.savetxt('result.out', results_log[0], delimiter=',')
-np.savetxt('maximum.out', maximum, delimiter=',')
+#np.savetxt('maximum.out', maximum, delimiter=',')
 #f = open("example.txt", "w")
 #f.write(repr(results_log[0]) + "\n")
 #f.close()
