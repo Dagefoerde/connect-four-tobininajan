e835d055488d99b730972d21905e3e697abc99f7
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-02-29 10:41:00 +0100

    Bots draw one after another

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ bot1 = ConnectFourBot()
 bot2 = ConnectFourBot()
 
 board = ConnectFourEnv()
-
+current_player = 1
 while not(board.is_win_state()):
-    move = bot1.nextMove()
-    print(move)
+    if current_player == 1:
+        move = bot1.nextMove()
+    else:
+        move = bot2.nextMove()
+    current_player *= -1
     if board.is_valid_action(move):
        board.step(move)
        bot1.inform(move)
