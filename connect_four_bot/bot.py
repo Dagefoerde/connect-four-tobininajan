7cfb428a918e023ae028a9ec6e1447db1d8e42bc
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-02-29 12:32:58 +0100

    Added NN bot

--- connect_four_bot/bot.py
+++ connect_four_bot/bot.py
@@ -1,12 +1,13 @@
-from typing import Tuple, Optional
-
 import gym
-import numpy as np
+import tensorflow as tf
 from random import shuffle
+from numpy import argmax
 
 from connect_four_bot.envs import ConnectFourEnv
+from connect_four_bot.linearmodel import Linear
 
-class ConnectFourBot(gym.Env):
+
+class ConnectFourBotRandom(gym.Env):
     """
     Description:
         ConnectFour game bot
@@ -18,6 +19,24 @@ class ConnectFourBot(gym.Env):
         x = list(self.board.available_moves())
         shuffle(x)
         return x[0]
+
+    def inform(self, action):
+        self.board.step(action)
+
+class ConnectFourBotNN(gym.Env):
+    """
+    Description:
+        ConnectFour game bot
+    """
+    def __init__(self, w, b):
+        self.board = ConnectFourEnv()
+        self.NN = Linear()
+
+    def nextMove(self):
+        boardvalues = self.board.board.flatten()
+        mytensor = tf.constant(boardvalues, dtype='float32')
+        x = self.NN([mytensor])
+        return argmax(x)
 
     def inform(self, action):
         self.board.step(action)
